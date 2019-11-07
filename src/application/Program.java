e6902f6c1da7ff44b36cc0b3acdd14494c448245
Valci Junior <valcijunior19@hotmail.com>
2019-11-06 22:48:39 -0300

    Tratamento de exceções e limpando o console

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoDoXadrez;
+import xadrez.XadrezExcecao;
 
 public class Program {
 
@@ -16,17 +18,33 @@ public class Program {
 		
 		while(true)
 		{
-			UI.imprimeTabuleiro(partida.getPecas());
-			System.out.println();
-			System.out.print("Posição de Origem: ");
-			PosicaoDoXadrez origem = UI.lendoAPosicao(tec);
-			
-			System.out.println();
-			System.out.print("Posição de Destino: ");
-			PosicaoDoXadrez destino = UI.lendoAPosicao(tec);
-			
-			PecaXadrez pecaCapturada = partida.movimento(origem, destino);
+			try
+			{
+				UI.limparTela();
+				UI.imprimeTabuleiro(partida.getPecas());
+				System.out.println();
+				System.out.print("Posição de Origem: ");
+				PosicaoDoXadrez origem = UI.lendoAPosicao(tec);
+				
+				System.out.println();
+				System.out.print("Posição de Destino: ");
+				PosicaoDoXadrez destino = UI.lendoAPosicao(tec);
+				
+				PecaXadrez pecaCapturada = partida.movimento(origem, destino);
+			}
+			catch(XadrezExcecao e)
+			{
+				System.out.println(e.getMessage());
+				tec.nextLine();
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println(e.getMessage());
+				tec.nextLine();
+			}
 		}
+		
+		
 	}
 
 }
