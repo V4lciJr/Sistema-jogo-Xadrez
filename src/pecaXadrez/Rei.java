bfb4eaaea959ec06200cb4e1ceda3cbf9b59fa1a
Valci Junior <valcijunior19@hotmail.com>
2019-11-15 00:23:23 -0300

    movimentos possíveis de um rei

--- src/pecaXadrez/Rei.java
+++ src/pecaXadrez/Rei.java
@@ -1,5 +1,6 @@
 package pecaXadrez;
 
+import jogotabuleiro.Posicao;
 import jogotabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -15,10 +16,75 @@ public class Rei extends PecaXadrez{
 	{
 		return "R";
 	}
+	
+	@SuppressWarnings("null")
+	private boolean podeMover(Posicao posicao)
+	{
+		PecaXadrez peca = (PecaXadrez) getTabuleiro().peca(posicao);
+		return peca == null || peca.getCor() != getCor();
+	}
 
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] matriz = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicao aux = new Posicao(0,0);
+		
+		// Movimento pra cima
+		aux.setValor(posicao.getLinha() - 1, posicao.getColuna());
+		if(getTabuleiro().posicaoExistente(aux) && podeMover(aux))
+		{
+			matriz[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Movimento pra baixo
+		aux.setValor(posicao.getLinha() + 1, posicao.getColuna());
+		if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) 
+		{
+			matriz[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Movimento pra esquerda
+		aux.setValor(posicao.getLinha(), posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) 
+		{
+			matriz[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Movimento pra direita
+		aux.setValor(posicao.getLinha(), posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) 
+		{
+			matriz[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Movimento pra noroeste
+		aux.setValor(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) 
+		{
+			matriz[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Movimento pra nordeste
+		aux.setValor(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) 
+		{
+			matriz[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Movimento sul
+		aux.setValor(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) 
+		{
+			matriz[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Movimento sudeste
+		aux.setValor(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) 
+		{
+			matriz[aux.getLinha()][aux.getColuna()] = true;
+		}
 		return matriz;
 	}
 }
